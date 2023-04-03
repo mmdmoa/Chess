da66fa00cc9f32e284f37a8a359713387e9ceebe
moein <mamadiii2023@gmail.com>
2023-04-04 00:31:56 +0430

    Added border and content_rect to Board

--- board.py
+++ board.py
@@ -3,10 +3,20 @@ from common_names import *
 class Board:
     def __init__(self,rect:FRect):
         self.rect = rect
-        self.border_size = 0
+        self.border_size = 10
         self.black_color = None
         self.white_color = None
 
+    @property
+    def content_rect( self ):
+        rect = self.rect.copy()
+        rect.x += self.border_size
+        rect.w -= self.border_size * 2
+        rect.y += self.border_size
+        rect.h -= self.border_size * 2
+
+        return rect
 
     def render( self,surface:Surface ):
-        pg.draw.rect(surface,[0,0,50],self.rect)
+        pg.draw.rect(surface,[0,0,50],self.content_rect)
+        pg.draw.rect(surface,[50,0,0],self.rect,width=self.border_size)
